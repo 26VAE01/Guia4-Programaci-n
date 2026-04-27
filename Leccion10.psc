ecfbc6a616a0d9f3b9cfd68f1ea0bde398ccca12
26VAE01 <ajcholonvianka16db@gmail.com>
2026-04-27 11:59:58 -0600

    Add files via upload

---
+++ Leccion10.psc
@@ -0,0 +1,15 @@
+Algoritmo Leccion10_Act1_Inc1
+	Definir i Como Entero
+	
+	Escribir "*********Copunera de Presi**********"
+	
+	Para i=1 Hasta 8 Con Paso 1 Hacer
+		Escribir "Presione un a tecla para marcar el sello número:",i
+		Esperar Tecla
+		Escribir "¡Sello ! ",i," mercado..."
+		Escribir "-----------------------------"
+	Fin Para
+	
+	Escribir "¡Felicidades! hasta completado lo s8 sellos."
+	Escribir "¡CORTE GRATIS!"
+FinAlgoritmo
---
+++ Leccion9C#.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double pesoAcumulado = 0;
+        double pesoManzana = 0;
+        const double META = 1000;
+        
+        while (pesoAcumulado<META)
+        {
+            Console.WriteLine("Faltan  {0}g para el kilo", META - pesoAcumulado);
+            Console.WriteLine("Ingrese el peso de la manzana en (100-300G)");
+
+            string entrada = Console.ReadLine();
+            if (double.TryParse(entrada, out pesoManzana))
+            {
+                if (pesoManzana >= 100 && pesoManzana <= 300)
+                {
+                    pesoAcumulado += pesoManzana;
+                    Console.WriteLine("Agreada. Total:{0}g", pesoAcumulado);
+                }
+                else
+                {
+                    Console.WriteLine("Error : La manzana no cumple con el peso");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Por favor, ingrese un número.");
+            }
+        }
+        Console.WriteLine("\n-------------------------------");
+        Console.WriteLine("Ya tiene el kilo de manzanas! total {0}g", pesoAcumulado);
+        Console.WriteLine("--------------------------------\n");
+
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+
+    }
+}
