c006ff7639a455db93fb813c565a32d8bba5d9b8
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 01:38:03 -0600

    Add files via upload

---
+++ Inciso1Pag95.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabla_multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+            }
+        }
+    }
+}
---
+++ Inciso1Pag97.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace multiplos_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(numero * i);
+            }
+        }
+    }
+}
