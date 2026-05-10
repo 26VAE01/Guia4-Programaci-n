0516f6f85c1d29c30f1c031b617f55ea98a3e974
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-09 23:53:58 -0600

    Add files via upload

---
+++ Inciso1Pag46.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace numeros_impares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 20; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
