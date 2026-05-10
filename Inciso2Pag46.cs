8bcdab9b4708a34306793d2dd3750739b55881d5
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-09 23:56:01 -0600

    Add files via upload

---
+++ Inciso2Pag46.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace numero_primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int contador = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    contador++;
+                }
+            }
+
+            if (contador == 2)
+            {
+                Console.WriteLine("El numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo");
+            }
+        }
+    }
+}
