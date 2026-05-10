5a64ab9e51c202428dbf0e5a6acf0c26d1013503
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 00:06:22 -0600

    Add files via upload

---
+++ Inciso1Pag49.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace multiplos_de_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i * 4);
+            }
+        }
+    }
+}
---
+++ Inciso2Pag49.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace numeros_pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
