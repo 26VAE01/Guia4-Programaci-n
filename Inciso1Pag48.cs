de2c2b538212efc4f6e6ce62a315a37564c26cfa
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 00:03:22 -0600

    Add files via upload

---
+++ Inciso1Pag48.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cuponera_peluqueria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sellos = 0;
+
+            Console.WriteLine("Ingrese la cantidad de sellos:");
+            sellos = Convert.ToInt32(Console.ReadLine());
+
+            while (sellos < 8)
+            {
+                Console.WriteLine("Aun no obtiene el corte gratis");
+
+                Console.WriteLine("Ingrese nuevamente la cantidad de sellos:");
+                sellos = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("********Corte de cabello gratis***********");
+        }
+    }
+}
