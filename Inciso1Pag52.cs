78d852644d3786e8b2947f8c8cedae07c191ef80
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 00:18:05 -0600

    Add files via upload

---
+++ Inciso1Pag52.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace estacionamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cajones = 20;
+
+            while (cajones > 0)
+            {
+                Console.WriteLine("Cajones disponibles: " + cajones);
+
+                cajones--;
+            }
+
+            Console.WriteLine("Ya no hay cajones libres");
+        }
+    }
+}
