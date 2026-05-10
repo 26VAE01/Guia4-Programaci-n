fabdb3008a95f0e293e0353d497cfbb1033831c8
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 01:16:40 -0600

    Add files via upload

---
+++ Inciso2Pag67.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace billetes_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int billete = 0;
+            int contador100 = 0;
+            int totalDinero = 0;
+            int cantidadBilletes = 0;
+
+            Console.WriteLine("Ingrese la cantidad de billetes:");
+            cantidadBilletes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= cantidadBilletes; i++)
+            {
+                Console.WriteLine("Ingrese el valor del billete:");
+                billete = Convert.ToInt32(Console.ReadLine());
+
+                if (billete == 100)
+                {
+                    contador100++;
+                    totalDinero = totalDinero + billete;
+                }
+            }
+
+            Console.WriteLine("Cantidad de billetes de 100: " + contador100);
+
+            Console.WriteLine("Total de dinero en billetes de 100: " + totalDinero);
+        }
+    }
+}
