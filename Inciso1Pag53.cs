e5070c16adc9c35056d2eafe1bd0c1b86c840789
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 00:32:14 -0600

    Add files via upload

---
+++ Inciso1Pag53.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace factorial_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int factorial = 1;
+
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("El factorial es: " + factorial);
+        }
+    }
+}
