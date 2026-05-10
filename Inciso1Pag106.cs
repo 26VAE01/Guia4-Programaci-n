da1c30d4607b261f239ccd521e799fd8f4a2f72f
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 02:01:03 -0600

    Add files via upload

---
+++ Inciso1Pag106.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace numeros_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorPrimos = 0;
+            int suma = 0;
+
+            for (int n = 1; n <= 100; n++)
+            {
+                int divisores = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        divisores++;
+                    }
+                }
+
+                if (divisores == 2)
+                {
+                    contadorPrimos++;
+                    suma = suma + n;
+                }
+            }
+
+            Console.WriteLine("Cantidad de numeros primos: " + contadorPrimos);
+
+            Console.WriteLine("Suma de numeros primos: " + suma);
+        }
+    }
+}
---
+++ Inciso2Pag106.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace billetes_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int billete = 0;
+            int cantidad100 = 0;
+            int totalDinero = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine("Ingrese el valor del billete:");
+                billete = Convert.ToInt32(Console.ReadLine());
+
+                if (billete == 100)
+                {
+                    cantidad100++;
+                    totalDinero = totalDinero + billete;
+                }
+            }
+
+            Console.WriteLine("Cantidad de billetes de 100: " + cantidad100);
+
+            Console.WriteLine("Total de dinero en billetes de 100: " + totalDinero);
+        }
+    }
+}
