6849d9c8c7ae097ca3b943211714c3b16896fa8d
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 00:56:45 -0600

    Add files via upload

---
+++ Inciso2Pagr54.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace suma_hasta_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int suma = 0;
+
+            while (num != 9)
+            {
+                Console.WriteLine("Ingrese un numero:");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                suma = suma + num;
+            }
+
+            Console.WriteLine("La suma total es: " + suma);
+        }
+    }
+}
