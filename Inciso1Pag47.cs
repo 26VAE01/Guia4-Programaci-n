a193b3a0e596d78995739a4d0ed9612159101a56
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-09 23:57:43 -0600

    Add files via upload

---
+++ Inciso1Pag47.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tabla_multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n + " x " + i + " = " + (n * i));
+            }
+        }
+    }
+}
+}
