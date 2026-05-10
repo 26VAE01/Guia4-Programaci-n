3abb7191440c62d6b48872b525f1e5c07f34a029
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 01:44:14 -0600

    Add files via upload

---
+++ Inciso1Pag98.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace suma_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            for (int n = 1; n <= 50; n++)
+            {
+                int divisores = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        divisores++;
+                    }
+                }
+
+                if (divisores == 2)
+                {
+                    suma = suma + n;
+                }
+            }
+
+            Console.WriteLine("La suma de los numeros primos es: " + suma);
+        }
+    }
+}
---
+++ Inciso2Pag98.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cuenta_regresiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int suma = 0;
+
+            Console.WriteLine("Ingrese un numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = numero; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+
+                suma = suma + i;
+            }
+
+            Console.WriteLine("La suma es: " + suma);
+        }
+    }
+}
