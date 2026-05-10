85c1453b19850658bf8a3b33eedf8f04191103a9
26VAE01 <ajcholonvianka16db@gmail.com>
2026-05-10 01:23:10 -0600

    Add files via upload

---
+++ Inciso1Pag68.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace multiplos_de_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int pares = 0;
+            int multiplo = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                multiplo = i * 7;
+
+                suma = suma + multiplo;
+
+                if (multiplo % 2 == 0)
+                {
+                    pares++;
+                }
+            }
+
+            Console.WriteLine("La sumatoria es: " + suma);
+
+            Console.WriteLine("Cantidad de multiplos pares: " + pares);
+        }
+    }
+}
---
+++ Inciso2Pag68.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace cuenta_regresiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorPrimos = 0;
+            int contadorPares = 0;
+
+            for (int n = 300; n >= 1; n--)
+            {
+                Console.WriteLine(n);
+
+                if (n % 2 == 0)
+                {
+                    contadorPares++;
+                }
+
+                int divisores = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        divisores++;
+                    }
+                }
+
+                if (divisores == 2)
+                {
+                    contadorPrimos++;
+                }
+            }
+
+            Console.WriteLine("Cantidad de numeros primos: " + contadorPrimos);
+
+            Console.WriteLine("Cantidad de numeros pares: " + contadorPares);
+        }
+    }
+}
